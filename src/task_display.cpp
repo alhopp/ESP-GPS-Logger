b76f1302b9b4aa19c460227efb29125096ec3d68
Alan Hopping <a.hopping@longreachcap.com>
2025-12-24 17:06:09 +0800

    Screen 1 2 3

--- src/task_display.cpp
+++ src/task_display.cpp
@@ -2,19 +2,52 @@
 
 #include <Arduino.h>
 
-#include "Ublox.h"
-#include "SD_card.h"
-#include "ESP_functions.h"
 #include "E_paper.h"
 #include "Fonts.h"
-#include "config_manager.h"
-#include "screen_system.h"
-
+#include "Definitions.h"
 
 // ----------------------------------------------------
 // Task handle (owned here)
 // ----------------------------------------------------
 TaskHandle_t t2 = nullptr;
+
+// ----------------------------------------------------
+// Display task (TEST MODE)
+// ----------------------------------------------------
+void taskTwo(void* parameter)
+{
+  LOG_TASK("Display", "test mode start");
+
+  int value = 1;
+
+  for (;;)
+  {
+    display.setFullWindow();
+    display.firstPage();
+    do {
+      display.fillScreen(GxEPD_WHITE);
+      display.setTextColor(GxEPD_BLACK);
+
+      display.setFont(Fonts::SpeedXL);   // big, obvious
+      display.setCursor(40, 120);
+      display.print(value);
+
+    } while (display.nextPage());
+
+    LOG_TASK("Display", "show %d", value);
+
+    value++;
+    if (value > 4) value = 1;
+
+    vTaskDelay(pdMS_TO_TICKS(2000)); // 2 seconds per number
+  }
+}
+
+/* ========================================================================== */
+/* ======================= ORIGINAL CODE (PARKED) =========================== */
+/* ========================================================================== */
+
+/*
 
 // ----------------------------------------------------
 // External state used by display task
@@ -29,46 +62,18 @@ static void handleLowBattery();
 static void updateDisplay();
 
 // ----------------------------------------------------
-// Display / UI task
+// Display / UI task (ORIGINAL)
 // ----------------------------------------------------
 void taskTwo(void* parameter)
 {
+  Serial.println("[TASK2] display task entered");
 
-
-     Serial.println("[TASK2] Display task disabled (stub)");
-
-  // TESTING TESTING Just idle forever 
-  while (true) {
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-  }
-
-
-
-   Serial.println("[TASK2] display task entered");
-
-      
-      //      display.setFont(&FreeSansBold18pt7b);
-
-    Boot_screen ();
-
-    // park task forever
-    for (;;) {
-      vTaskDelay(pdMS_TO_TICKS(1000));
-    }
-    Serial.println("[TASK2] display refresh done");
-
-  
+  Boot_screen();
 
   while (true) {
 
-    // ------------------------------------------------
-    // Watchdog heartbeat
-    // ------------------------------------------------
     wdt_task1 = millis();
 
-    // ------------------------------------------------
-    // Auto-cycle stat screens
-    // ------------------------------------------------
     if (config.Stat_screens_time != 0) {
       stat_count++;
     }
@@ -78,18 +83,12 @@ void taskTwo(void* parameter)
 
     Update_bat();
 
-    // ------------------------------------------------
-    // Battery supervision
-    // ------------------------------------------------
     if (RTC_voltage_bat < RTC_minimum_voltage_bat) {
       low_bat_count++;
     } else {
       low_bat_count = 0;
     }
 
-    // ------------------------------------------------
-    // Power-down paths
-    // ------------------------------------------------
     if (sleep_mode) {
       handleSleep();
     }
@@ -102,56 +101,5 @@ void taskTwo(void* parameter)
   }
 }
 
-static void handleSleep()
-{
-  Ublox_off();
-  Off_screen(RTC_OFF_screen);
-  Serial.println("RTC_OFF_screen");
-  delay(2000);
-  Shut_down();
-  delay(100);
-  vTaskDelete(nullptr);
-}
+*/
 
-static void handleLowBattery()
-{
-  sleep_mode = true;
-
-  char tekst[32];
-  sprintf(tekst, "Shutdown low bat @ %.1f V\n", RTC_minimum_voltage_bat);
-  logERR(tekst);
-
-  Off_screen(2); // shutdown low battery screen
-  delay(2000);
-  Shut_down();
-  delay(100);
-  vTaskDelete(nullptr);
-}
-
-static void updateDisplay()
-{
-  if (millis() < 2000) {
-    Update_screen(BOOT_SCREEN);
-  }
-  else if (trouble_screen) {
-    Update_screen(TROUBLE);
-  }
-  else if (!GPS_Signal_OK) {
-    Update_screen(WIFI_ON);
-  }
-  else if (!Time_Set_OK) {
-    Update_screen(WIFI_ON);
-  }
-#if defined(GPIO12_ACTIF)
-  else if (Short_push12.long_pulse) {
-    Update_screen(config.gpio12_screen[GPIO12_screen]);
-  }
-#endif
-  else if ((gps_speed / 1000.0f < config.stat_speed) && !Field_choice) {
-    Update_screen(config.stat_screen[stat_count]);
-  }
-  else {
-    Update_screen(SPEED);
-    if (config.Stat_screens_time != 0) stat_count = 0;
-  }
-}
