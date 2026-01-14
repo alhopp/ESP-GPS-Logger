919a4561027244e41eb1760e2eb8adc096b4e78f
Alan Hopping <a.hopping@longreachcap.com>
2026-01-14 11:41:16 +0800

    looking grat before bi code cleanup

--- src/system_info.cpp
+++ src/system_info.cpp
@@ -1,14 +1,13 @@
 #include "system_info.h"
 
 const SystemInfo systemInfo = {
-  .gnss_module     = "u-blox NEO-M10",
-  .storage_mb      = 128,
-  .software_version= "Version 1",
+  .gnss_module          = "u-blox NEO-M10",
+  .storage_mb           = 128,
+  .software_version     = "Version 1",
 
-  .speed_units     = "Knots",
-  .sample_rate     = "5 Hz",
-  .gnss_mode       = "GPS + GLONASS + GALILEO",
-  .dynamic_model   = "SEA",
-  .display         = "LilyGO T5 B74"
-
+  .speed_units          = "Knots",
+  .sample_rate          = 5,
+  .gnss_mode            = "GPS + GLONASS + GALILEO",
+  .dynamic_model        = "SEA",
+  .display              = "LilyGO T5 B74"
 };
