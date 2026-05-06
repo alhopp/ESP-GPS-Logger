d38b609e8ed831e18dd69b1cf816cc5afdddba61
Alan Hopping <a.hopping@longreachcap.com>
2026-05-06 14:04:14 +0800

    Checkpoint 37

--- src/Display/Screens/screen_boot.cpp
+++ src/Display/Screens/screen_boot.cpp
@@ -1,6 +1,8 @@
 #include "Display/Screens/screen_system.h"
 
 #include "Core/Rtc/rtc_battery_state.h"
+#include "Core/Battery/battery_monitor.h"
+#include "Display/display_battery.h"
 #include "Display/Bitmaps/display_bitmaps.h"
 #include "Display/E_paper.h"
 #include "Display/Screens/screen_system_common.h"
@@ -19,4 +21,12 @@ void draw_BOOT()
   display.drawBitmap(200, 3, ESP_GPS_logo, 48, 48, GxEPD_WHITE, GxEPD_BLACK);
   drawCenteredText("ESP-GPS", Layout::ROW9(3), Fonts::Body12);
   drawCenteredText("Initialising system", Layout::ROW9(5), Fonts::Body9);
+
+  display.setFont(Fonts::Body9);
+  display.setCursor(42, Layout::ROW9(7));
+  display.print("Battery ");
+  display.print(battery_display_voltage(RTC_voltage_bat), 1);
+  display.print("V ");
+  display.print(static_cast<int>(displayBatteryPercent(RTC_voltage_bat)));
+  display.print("%");
 }
