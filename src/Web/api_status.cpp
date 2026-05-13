aabdfd0ffa0591257663fca990bae5b76f6a2de7
Alan Hopping <a.hopping@longreachcap.com>
2026-05-13 09:30:15 +0800

    Group status API fields

--- src/Web/api_status.cpp
+++ src/Web/api_status.cpp
@@ -15,10 +15,32 @@
 
 namespace {
 
+bool wifiConnected()
+{
+  return WiFi.status() == WL_CONNECTED;
+}
+
+void addGpsStatus(JsonDocument& j)
+{
+  j["gps_lifecycle"] = gps_lifecycle_state_name();
+  j["gps_signal_ok"] = GPS_Signal_OK;
+  j["satellites"] = ubxMessage.navPvt.numSV;
+  j["fix_type"] = ubxMessage.navPvt.fixType;
+  j["last_gps_age_ms"] = last_gps_msg ? millis() - last_gps_msg : -1;
+}
+
+void addStorageStatus(JsonDocument& j)
+{
+  j["sd_ok"] = storage_sd_available();
+  j["sd_mounted"] = storage_sd_mounted();
+  j["littlefs_ok"] = storage_littlefs_available();
+  j["storage_shutting_down"] = storage_is_shutting_down();
+}
+
 void handleNetStatus(WebServer& server)
 {
   StaticJsonDocument<256> j;
-  j["connected"] = WiFi.status() == WL_CONNECTED;
+  j["connected"] = wifiConnected();
   j["ssid"] = WiFi.SSID();
   j["ip"] = WiFi.localIP().toString();
   web_send_json(server, j);
@@ -29,17 +51,10 @@ void handleStatus(WebServer& server)
   StaticJsonDocument<768> j;
   j["mode"] = modeToString(getMode());
   j["session_active"] = logging_session_active();
-  j["gps_lifecycle"] = gps_lifecycle_state_name();
-  j["gps_signal_ok"] = GPS_Signal_OK;
-  j["satellites"] = ubxMessage.navPvt.numSV;
-  j["fix_type"] = ubxMessage.navPvt.fixType;
-  j["last_gps_age_ms"] = last_gps_msg ? millis() - last_gps_msg : -1;
-  j["sd_ok"] = storage_sd_available();
-  j["sd_mounted"] = storage_sd_mounted();
-  j["littlefs_ok"] = storage_littlefs_available();
-  j["storage_shutting_down"] = storage_is_shutting_down();
+  addGpsStatus(j);
+  addStorageStatus(j);
   j["simulator"] = build_gps_simulator_enabled();
-  j["wifi_connected"] = WiFi.status() == WL_CONNECTED;
+  j["wifi_connected"] = wifiConnected();
   web_send_json(server, j);
 }
 
